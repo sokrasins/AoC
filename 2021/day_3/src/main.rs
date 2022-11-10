3016af18a6646138fd5c525e13f83462f4a323cd
Stan <sokrasins@gmail.com>
2022-11-10 00:11:24 -0500

    day 3 pt 1:

--- 2021/day_3/src/main.rs
+++ 2021/day_3/src/main.rs
@@ -1,19 +1,73 @@
 use std::fs;
+use std::vec::Vec;
 
-const FILE: &str = "test.txt";
-//const FILE: &str = "final.txt";
+//const FILE: &str = "test.txt";
+const FILE: &str = "final.txt";
 
-fn calc_gamma(readings: &str) -> (u32, u32) {
-    for line in readings.lines() {
-        let new_val = u32::from_str_radix(line, 2).unwrap();
-        println!("{}", new_val);
-        println!("{}", line.len());
+struct Diagnostic {
+    ones: Vec<usize>,
+    zeros: Vec<usize>,
+}
+
+impl Diagnostic {
+    pub fn new(num_digits: usize) -> Self {
+        let mut ones: Vec<usize> = Vec::new();
+        let mut zeros: Vec<usize> = Vec::new();
+
+        ones.resize(num_digits, 0);
+        zeros.resize(num_digits, 0);
+
+        Diagnostic { ones, zeros }
     }
 
-    (0, 0)
+    pub fn from_reading(line: &str) -> Self {
+        let mut diagnostic = Self::new(line.len());
+        diagnostic.add_reading(line);
+
+        diagnostic
+    }
+
+    pub fn add_reading(&mut self, line: &str) {
+        for (i, char) in line.chars().rev().enumerate() {
+            match char {
+                '0' => self.zeros[i] += 1,
+                '1' => self.ones[i] += 1,
+                _ => ()
+            }
+        }
+    }
+
+    pub fn gamma(&mut self) -> u32 {
+        let mut gamma: u32 = 0;
+        for i in 0..self.ones.len() {
+            if self.ones[i] > self.zeros[i] {
+                gamma |= 1 << i;
+            }
+        }    
+        gamma
+    }
+
+    pub fn epsilon(&mut self) -> u32 {
+        let gamma = self.gamma();
+        let mask: u32 = (2 as u32).pow(self.ones.len() as u32) - 1;
+
+        gamma ^ mask
+    }
+}
+
+fn calc_diagnostic(readings: &str) -> (u32, u32) {
+    let mut line_iter = readings.lines();
+    
+    let mut diagnostic = Diagnostic::from_reading(line_iter.next().unwrap());
+    for line in line_iter {
+        diagnostic.add_reading(line);
+    }
+
+    (diagnostic.gamma(), diagnostic.epsilon())
 }
 
 fn main() {
     let contents = fs::read_to_string(FILE).unwrap();    
-    let (gamma, epsilon) = calc_gamma(&contents);
+    let (gamma, epsilon) = calc_diagnostic(&contents);
+    println!("power: {}", gamma * epsilon);
 }

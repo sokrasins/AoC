10eb2233e84c09de6dbd317fc4010a4f09151218
Stan <sokrasins@gmail.com>
2022-11-13 20:32:39 -0500

    Finish day 6

--- 2021/day_6/src/main.rs
+++ 2021/day_6/src/main.rs
@@ -1,10 +1,54 @@
 use std::fs;
-//use std::vec::Vec;
 
-const FILE: &str = "test.txt";
-//const FILE: &str = "final.txt";
+//const FILE: &str = "test.txt";
+const FILE: &str = "final.txt";
+
+const SPAWN_TIME: usize = 6;
+const BIRTH_DELAY: usize = 2;
+const MAX_DAYS: usize = SPAWN_TIME + BIRTH_DELAY;
+
+const STEPS: usize = 256;
+
+fn age(school: &mut [u64]) {
+    let temp: u64 = school[0];
+
+    for i in 0..MAX_DAYS {
+        school[i] = school[i+1];
+    }
+
+    // All fish in slot 0 spawn new fish
+    school[MAX_DAYS] = temp;
+
+    // All fish in slot 0 reset their spawn counter
+    school[SPAWN_TIME] += temp;
+}
+
+fn sum(school: &[u64]) -> u64 {
+    let mut tot: u64 = 0;
+    for i in school.iter() {
+        tot += i; 
+    }
+    tot
+}
 
 fn main() {
     let contents = fs::read_to_string(FILE).unwrap();
     let mut lines = contents.lines();
+
+    let mut school: [u64; MAX_DAYS + 1] = [0; MAX_DAYS + 1];
+
+    let init = lines
+        .next()
+        .unwrap()
+        .split(",");
+    
+    for i in init {
+        school[i.parse::<usize>().unwrap()] += 1;
+    }
+
+    for _ in 0..STEPS {
+        age(&mut school);
+    }
+
+    println!("{}", sum(&school));
 }
